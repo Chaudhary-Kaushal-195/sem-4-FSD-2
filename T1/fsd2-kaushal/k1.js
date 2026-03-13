51c1ab0029f9598206109308873d19de5e7e1e6a
Kaushal Chaudhary <kaushalchau.2007@gmail.com>
2026-03-13 21:46:04 +0530

    go

---
+++ T1/fsd2-kaushal/k1.js
@@ -0,0 +1,14 @@
+var fs= require("fs")
+// fs.writeFileSync("hello.txt","Good morning")
+// fs.appendFileSync("hello.txt"," it is warm day")
+// fs.appendFileSync("h1.txt"," it is warm day")
+// var data= fs.readFileSync("hello.txt")
+// console.log(data)
+// console.log(data.toString())
+// console.log('-------------------------------------------------')
+// var data1= fs.readFileSync("hello.txt","utf-8")
+// console.log(data1)
+
+// fs.renameSync("hello.txt","h2.txt")
+
+fs.unlinkSync("h1.txt")
---
+++ T1/fsd2-kaushal/script.js
@@ -0,0 +1,12 @@
+ var obj={
+        "name":"abc",
+        "age":30,
+        "isPass":true,
+        "address":{"city":"Ahmadabad","ZIP":380058},
+        "subject":["FSD","python","TOC"],
+        "iscalled":null
+    }
+    console.log(obj.age)
+    console.log(obj.address)
+    console.log(obj.address.ZIP)
+    console.log(obj.subject[1])
